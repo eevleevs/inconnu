115be189d09b3450d2e75eb102ebf1439ce975be
Malventi, Giulio <Giulio.Malventi@nov.com>
2022-08-18 03:29:27 +0200

    Upgrade jose

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export {encode} from 'https://deno.land/std@0.151.0/node/querystring.ts'
-export {generateSecret, jwtVerify, SignJWT} from 'https://deno.land/x/jose@v4.8.3/index.ts'
+export {generateSecret, jwtVerify, SignJWT} from 'https://deno.land/x/jose@v4.9.0/index.ts'
 export {Router, opine} from 'https://deno.land/x/opine@2.2.0/mod.ts'

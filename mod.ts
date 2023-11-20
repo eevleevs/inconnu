fed3d0f78cf4ccf20856fc5171a8b4bcc10a34b6
Giulio Malventi <giulio.malventi@gmail.com>
2023-11-20 09:32:12 +0100

    Use expiring_map from deno.land

--- mod.ts
+++ mod.ts
@@ -1,9 +1,20 @@
-if ('readTextFileSync' in Deno) await import('https://deno.land/std@0.153.0/dotenv/load.ts')
-import { getCookies } from "https://deno.land/std@0.153.0/http/cookie.ts";
-import { encode } from 'https://deno.land/std@0.153.0/node/querystring.ts'
-import { generateSecret, JWTPayload, jwtVerify, SignJWT } from 'https://deno.land/x/jose@v4.9.1/index.ts'
-import { opine, OpineRequest, OpineResponse, Router } from 'https://deno.land/x/opine@2.3.3/mod.ts'
-import { ExpiringMap } from './expiring_map.ts'
+if ('readTextFileSync' in Deno)
+  await import('https://deno.land/std@0.153.0/dotenv/load.ts')
+import {getCookies} from 'https://deno.land/std@0.153.0/http/cookie.ts'
+import {encode} from 'https://deno.land/std@0.153.0/node/querystring.ts'
+import {
+  generateSecret,
+  JWTPayload,
+  jwtVerify,
+  SignJWT,
+} from 'https://deno.land/x/jose@v4.9.1/index.ts'
+import {
+  opine,
+  OpineRequest,
+  OpineResponse,
+  Router,
+} from 'https://deno.land/x/opine@2.3.3/mod.ts'
+import {ExpiringMap} from 'https://deno.land/x/expiring_map@1.0.0/mod.ts'
 
 export interface Provider {
   getAuthCodeUrl(query: object, origin: string): Promise<string>
@@ -21,55 +32,64 @@ const secrets = new ExpiringMap(300000)
 // common functions
 const origin = (req: OpineRequest) =>
   `http${req.get('host')?.match(/^localhost:/) ? '' : 's'}://${req.get('host')}`
-const sign = (payload: JWTPayload) => new SignJWT(payload)
-  .setProtectedHeader({alg: 'HS256'})
-  .setExpirationTime(expiration)
-  .sign(jwk)
+const sign = (payload: JWTPayload) =>
+  new SignJWT(payload)
+    .setProtectedHeader({alg: 'HS256'})
+    .setExpirationTime(expiration)
+    .sign(jwk)
 const verify = (req: OpineRequest, res: OpineResponse) => {
-  const jwt = req.headers.get('authorization')?.match(/^Bearer (\S+)/)?.at(1)
-    ?? getCookies(req.headers)['inconnu-auth']
+  const jwt =
+    req.headers
+      .get('authorization')
+      ?.match(/^Bearer (\S+)/)
+      ?.at(1) ?? getCookies(req.headers)['inconnu-auth']
   jwtVerify(jwt, jwk)
     .then(result => res.json(result.payload))
     .catch(err => res.setStatus(401).send(err))
 }
 
 // method and route logging
-if (Deno.env.get('INCONNU_LOG')) app.use((req, _res, next) => {
-  console.log(`${req.method} ${req.url}`)
-  next()
-})
+if (Deno.env.get('INCONNU_LOG'))
+  app.use((req, _res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+  })
 
 if (hubUrl) {
   // satellite setup
-  app.use(Deno.env.get('INCONNU_SAT_PATH') || '/inconnu', new Router()
-    .get('/authenticate', (req, res) => {
-      const satSecret = crypto.randomUUID()
-      secrets.set(satSecret, true)
-      res.redirect(`${hubUrl}/authenticate?` + encode({
-        ...req.query,
-        receiver: origin(req) + '/inconnu/authenticated',
-        satSecret,
-      }))
-    })
-    .get('/authenticated', async (req, res) => {
-      if (!secrets.delete(req.query.satSecret)) return res.sendStatus(401)
-      const result = await fetch(`${hubUrl}/redeem?` + encode(req.query))
-      if (!result.ok) return res.sendStatus(401)
-      const jwt = await sign(await result.json())
-      res.cookie({
-        name: 'inconnu-auth',
-        value: jwt,
-        expires: new Date(JSON.parse(atob(jwt.split('.')[1])).exp * 1000),
-        httpOnly: true,
-        sameSite: 'Lax',
+  app.use(
+    Deno.env.get('INCONNU_SAT_PATH') || '/inconnu',
+    new Router()
+      .get('/authenticate', (req, res) => {
+        const satSecret = crypto.randomUUID()
+        secrets.set(satSecret, true)
+        res.redirect(
+          `${hubUrl}/authenticate?` +
+            encode({
+              ...req.query,
+              receiver: origin(req) + '/inconnu/authenticated',
+              satSecret,
+            }),
+        )
       })
-      res.redirect(req.query.redirect || origin(req) + '/inconnu/verify')
-    })
-    .get('/logout', (_req, res) => res
-      .clearCookie('inconnu-auth')
-      .redirect(`${hubUrl}/logout`)
-    )
-    .get('/verify', verify)
+      .get('/authenticated', async (req, res) => {
+        if (!secrets.delete(req.query.satSecret)) return res.sendStatus(401)
+        const result = await fetch(`${hubUrl}/redeem?` + encode(req.query))
+        if (!result.ok) return res.sendStatus(401)
+        const jwt = await sign(await result.json())
+        res.cookie({
+          name: 'inconnu-auth',
+          value: jwt,
+          expires: new Date(JSON.parse(atob(jwt.split('.')[1])).exp * 1000),
+          httpOnly: true,
+          sameSite: 'Lax',
+        })
+        res.redirect(req.query.redirect || origin(req) + '/inconnu/verify')
+      })
+      .get('/logout', (_req, res) =>
+        res.clearCookie('inconnu-auth').redirect(`${hubUrl}/logout`),
+      )
+      .get('/verify', verify),
   )
   console.log(`satellite mode, using hub ${hubUrl}`)
 } else {
@@ -79,31 +99,41 @@ if (hubUrl) {
     const name = file.name.match(/(.*)\.ts$/)?.at(1)
     if (!name || !Deno.env.get(`INCONNU_${name.toUpperCase()}`)) continue
     names.push(name)
-    const provider: Provider = (await import('./providers/' + file.name)).provider
-    app.use(`/${name}`, new Router()
-      .get('/authenticate', async (req, res) => {
-        const hubSecret = crypto.randomUUID()
-        secrets.set(hubSecret, true)
-        res.redirect(await provider.getAuthCodeUrl({...req.query, hubSecret}, origin(req)))
-      })
-      .get('/authenticated', (req, res) => {
-        const {hubSecret, receiver, ...state} = JSON.parse(req.query.state)
-        if (!secrets.delete(hubSecret)) return res.sendStatus(401)
-        const code = crypto.randomUUID()
-        secrets.set(code, req.query)
-        res.redirect((receiver || `/${name}/redeem`) + '?' + encode({...state, code}))
-      })
-      .get('/logout', (_req, res) => res.redirect(provider.getLogoutUrl()))
-      .get('/redeem', async (req, res) => {
-        const query = secrets.pop(req.query.code)
-        if (!query) return res.sendStatus(401)
-        const payload = await provider.getPayload(query)
-        res.json({
-          ...payload,
-          ...req.query.jwt && {jwt: await sign(payload)},
+    const provider: Provider = (await import('./providers/' + file.name))
+      .provider
+    app.use(
+      `/${name}`,
+      new Router()
+        .get('/authenticate', async (req, res) => {
+          const hubSecret = crypto.randomUUID()
+          secrets.set(hubSecret, true)
+          res.redirect(
+            await provider.getAuthCodeUrl(
+              {...req.query, hubSecret},
+              origin(req),
+            ),
+          )
         })
-      })
-      .get('/verify', verify)
+        .get('/authenticated', (req, res) => {
+          const {hubSecret, receiver, ...state} = JSON.parse(req.query.state)
+          if (!secrets.delete(hubSecret)) return res.sendStatus(401)
+          const code = crypto.randomUUID()
+          secrets.set(code, req.query)
+          res.redirect(
+            (receiver || `/${name}/redeem`) + '?' + encode({...state, code}),
+          )
+        })
+        .get('/logout', (_req, res) => res.redirect(provider.getLogoutUrl()))
+        .get('/redeem', async (req, res) => {
+          const query = secrets.pop(req.query.code)
+          if (!query) return res.sendStatus(401)
+          const payload = await provider.getPayload(query)
+          res.json({
+            ...payload,
+            ...(req.query.jwt && {jwt: await sign(payload)}),
+          })
+        })
+        .get('/verify', verify),
     )
   }
   console.log('hub mode, using ' + names.join(', '))

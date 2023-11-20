a217d438928818bd74b7a6fb9ff235d18168d380
Giulio Malventi <giulio.malventi@gmail.com>
2023-11-20 13:04:41 +0100

    Initial

--- mod.ts
+++ mod.ts
@@ -17,21 +17,20 @@ import {
 import {ExpiringMap} from 'https://deno.land/x/expiring_map@1.0.0/mod.ts'
 
 export interface Provider {
-  getAuthCodeUrl(query: object, origin: string): Promise<string>
+  getAuthCodeUrl(query: object, redirect: string): Promise<string>
   getLogoutUrl(): string
   getPayload(query: any): Promise<JWTPayload>
 }
 
 const app = opine()
 const expiration = Deno.env.get('INCONNU_JWT_EXPIRATION') || '1w'
-const hubUrl = Deno.env.get('INCONNU_HUB_URL')
 const jwk = await generateSecret('HS256')
+const path = Deno.env.get('INCONNU_PATH') ?? '/inconnu'
 const port = 3001
+const mainRouter = new Router()
 const secrets = new ExpiringMap(300000)
 
 // common functions
-const origin = (req: OpineRequest) =>
-  `http${req.get('host')?.match(/^localhost:/) ? '' : 's'}://${req.get('host')}`
 const sign = (payload: JWTPayload) =>
   new SignJWT(payload)
     .setProtectedHeader({alg: 'HS256'})
@@ -55,93 +54,65 @@ if (Deno.env.get('INCONNU_LOG'))
     next()
   })
 
-if (hubUrl) {
-  // satellite setup
-  app.use(
-    Deno.env.get('INCONNU_SAT_PATH') || '/inconnu',
+// providers setup
+const names = []
+for await (const file of Deno.readDir('providers')) {
+  const name = file.name.match(/(.*)\.ts$/)?.at(1)
+  if (!name || !Deno.env.get(`INCONNU_${name.toUpperCase()}`)) continue
+  names.push(name)
+  const provider: Provider = (await import('./providers/' + file.name)).provider
+  mainRouter.use(
+    `/${name}`,
     new Router()
-      .get('/authenticate', (req, res) => {
-        const satSecret = crypto.randomUUID()
-        secrets.set(satSecret, true)
+      .get('/authenticate', async (req, res) => {
+        const secret = crypto.randomUUID()
+        secrets.set(secret, true)
         res.redirect(
-          `${hubUrl}/authenticate?` +
-            encode({
-              ...req.query,
-              receiver: origin(req) + '/inconnu/authenticated',
-              satSecret,
-            }),
+          await provider.getAuthCodeUrl(
+            {...req.query, secret},
+            `${req.protocol}://${req.hostname}${path}/${name}/authenticated`,
+          ),
         )
       })
-      .get('/authenticated', async (req, res) => {
-        if (!secrets.delete(req.query.satSecret)) return res.sendStatus(401)
-        const result = await fetch(`${hubUrl}/redeem?` + encode(req.query))
-        if (!result.ok) return res.sendStatus(401)
-        const jwt = await sign(await result.json())
-        res.cookie({
-          name: 'inconnu-auth',
-          value: jwt,
-          expires: new Date(JSON.parse(atob(jwt.split('.')[1])).exp * 1000),
-          httpOnly: true,
-          sameSite: 'Lax',
-        })
-        res.redirect(req.query.redirect || origin(req) + '/inconnu/verify')
+      .get('/authenticated', (req, res) => {
+        const {secret, receiver, ...state} = JSON.parse(req.query.state)
+        if (!secrets.delete(secret)) return res.sendStatus(401)
+        const code = crypto.randomUUID()
+        secrets.set(code, req.query)
+        res.redirect(
+          (receiver || `/${name}/redeem`) + '?' + encode({...state, code}),
+        )
       })
       .get('/logout', (_req, res) =>
-        res.clearCookie('inconnu-auth').redirect(`${hubUrl}/logout`),
+        res.clearCookie('inconnu-auth').redirect(provider.getLogoutUrl()),
       )
+      .get('/redeem', async (req, res) => {
+        const query = secrets.pop(req.query.code)
+        if (!query) return res.sendStatus(401)
+        const payload = await provider.getPayload(query)
+        const jwt = await sign(payload)
+        res
+          .cookie({
+            name: 'inconnu-auth',
+            value: jwt,
+            expires: new Date(JSON.parse(atob(jwt.split('.')[1])).exp * 1000),
+            httpOnly: true,
+            sameSite: 'Lax',
+          })
+          .json({
+            ...payload,
+            jwt,
+          })
+      })
       .get('/verify', verify),
   )
-  console.log(`satellite mode, using hub ${hubUrl}`)
-} else {
-  // hub setup
-  const names = []
-  for await (const file of Deno.readDir('providers')) {
-    const name = file.name.match(/(.*)\.ts$/)?.at(1)
-    if (!name || !Deno.env.get(`INCONNU_${name.toUpperCase()}`)) continue
-    names.push(name)
-    const provider: Provider = (await import('./providers/' + file.name))
-      .provider
-    app.use(
-      `/${name}`,
-      new Router()
-        .get('/authenticate', async (req, res) => {
-          const hubSecret = crypto.randomUUID()
-          secrets.set(hubSecret, true)
-          res.redirect(
-            await provider.getAuthCodeUrl(
-              {...req.query, hubSecret},
-              origin(req),
-            ),
-          )
-        })
-        .get('/authenticated', (req, res) => {
-          const {hubSecret, receiver, ...state} = JSON.parse(req.query.state)
-          if (!secrets.delete(hubSecret)) return res.sendStatus(401)
-          const code = crypto.randomUUID()
-          secrets.set(code, req.query)
-          res.redirect(
-            (receiver || `/${name}/redeem`) + '?' + encode({...state, code}),
-          )
-        })
-        .get('/logout', (_req, res) => res.redirect(provider.getLogoutUrl()))
-        .get('/redeem', async (req, res) => {
-          const query = secrets.pop(req.query.code)
-          if (!query) return res.sendStatus(401)
-          const payload = await provider.getPayload(query)
-          res.json({
-            ...payload,
-            ...(req.query.jwt && {jwt: await sign(payload)}),
-          })
-        })
-        .get('/verify', verify),
-    )
-  }
-  console.log('hub mode, using ' + names.join(', '))
 }
+console.log('using ' + names.join(', '))
 
 // generic routes and start
-app
+mainRouter
   .get('/', (_req, res) => res.redirect('https://github.com/eevleevs/inconnu'))
   .get('/verify', verify)
-  .listen(port)
+
+app.set('trust proxy', true).use(path, mainRouter).listen(port)
 console.log('listening on port ' + port)
--- providers/microsoft.ts
+++ providers/microsoft.ts
@@ -1,37 +1,46 @@
-import { ConfidentialClientApplication } from 'https://denopkg.com/eevleevs/azure-msal-deno@master/mod.ts'
-import { Provider } from '../mod.ts'
+import {ConfidentialClientApplication} from 'https://denopkg.com/eevleevs/azure-msal-deno@master/mod.ts'
+import {Provider} from '../mod.ts'
 
 class CustomClientApplication extends ConfidentialClientApplication {
   getLogoutUrl = () => this.config.auth.authority + 'oauth2/v2.0/logout'
 }
-  
-const [clientId, clientSecret] = (Deno.env.get('INCONNU_MICROSOFT') ?? '').split(':')
+
+const [clientId, clientSecret] = (
+  Deno.env.get('INCONNU_MICROSOFT') ?? ''
+).split(':')
 const cca = new CustomClientApplication({auth: {clientId, clientSecret}})
 const scopes = ['user.read', 'directory.read.all']
 let redirectUri: string
 
 export const provider: Provider = {
-  getAuthCodeUrl: (query, origin) => cca.getAuthCodeUrl({
-    redirectUri: redirectUri ??= origin + '/microsoft/authenticated',
-    scopes,
-    state: JSON.stringify(query)
-  }),
+  getAuthCodeUrl: (query, redirect) =>
+    cca.getAuthCodeUrl({
+      redirectUri: (redirectUri ??= redirect),
+      scopes,
+      state: JSON.stringify(query),
+    }),
   getLogoutUrl: cca.getLogoutUrl,
   getPayload: async query => {
-    const acquired = await cca.acquireTokenByCode({code: query.code, redirectUri, scopes})
+    const acquired = await cca.acquireTokenByCode({
+      code: query.code,
+      redirectUri,
+      scopes,
+    })
     const payload: any = {username: acquired?.account?.username?.toLowerCase()}
     const state = JSON.parse(query.state)
     if (state.memberOf) {
-      const groups = (await (await fetch(
-        'https://graph.microsoft.com/v1.0/me/memberOf?$select=displayName&$top=999',
-        {headers: {authorization: 'Bearer ' + acquired?.accessToken}}
-      )).json())
-        .value
-        .map((v: any) => v.displayName)
+      const groups = (
+        await (
+          await fetch(
+            'https://graph.microsoft.com/v1.0/me/memberOf?$select=displayName&$top=999',
+            {headers: {authorization: 'Bearer ' + acquired?.accessToken}},
+          )
+        ).json()
+      ).value.map((v: any) => v.displayName)
       payload.memberOf = state.memberOf
         .split(',')
         .filter((group: string) => groups.includes(group))
     }
     return payload
-  }
+  },
 }
